103af26d7c0c824c2b232d813f182a358eac9f73
Janina86 <janinabolba@gmail.com>
2024-07-21 17:52:18 +0300

    script.js

--- script.js
+++ script.js
@@ -1,19 +1,17 @@
-document.addEventListener("DOMContentLoaded", function() {
-  let buttonIds = ["button1", "button2", "button3"];
-  let randomIndex = Math.floor(Math.random() * buttonIds.length);
-  let winningButtonId = buttonIds[randomIndex];
+let buttonIds = ["button1", "button2", "button3"];
+let randomIndex = Math.floor(Math.random() * buttonIds.length);
+let winningButtonId = buttonIds[randomIndex];
 
-  function handleButtonClick(event) {
-    let buttonId = event.target.id;
-    if (buttonId === winningButtonId) {
-      alert("You win!");
-    } else {
-      alert("You lose!");
-    }
+function handleButtonClick(event) {
+  let buttonId = event.target.id;
+  if (buttonId === winningButtonId) {
+    alert("You win!");
+  } else {
+    alert("You lose!");
   }
+}
 
-  buttonIds.forEach(function(buttonId) {
-    let button = document.getElementById(buttonId);
-    button.addEventListener("click", handleButtonClick);
-  });
+buttonIds.forEach(function(buttonId) {
+  let button = document.getElementById(buttonId);
+  button.addEventListener("click", handleButtonClick);
 });
